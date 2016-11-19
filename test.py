e34226c0e2d8055e2875ab15ec7bb5a247e634ff
johanv303 <jhviljoen303@gmail.com>
2016-11-19 20:04:03 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+var = 123
+my_variable = "this is my string"
+
+my_second_variable = "and this is more"
+
+my_combined_variable = my_variable + " " + my_second_variable
+
+x = "what is this"
+
+print(x)
+
+print("hello world")
+
+x = "this is a change"
+
+print (my_combined_variable)
